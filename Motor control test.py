09a96629ec041e2222ebd71d42f35d994e7053c2
kimgyeongheon1 <alswls6758@naver.com>
2024-11-22 05:41:38 +0900

    모터 전진 후진 좌회전 우회전 테스트 코드

--- Motor control test.py
+++ Motor control test.py
@@ -32,8 +32,32 @@ class Motor:
 board = Arduino('/dev/ttyACM0')
 it = util.Iterator(board)
 
+
+motor1_Lpwm = 5
+motor1_Rpwm = 6
+motor1_Len = 2  
+motor1_Ren = 4 
+
+motor2_Lpwm = 9
+motor2_Rpwm = 10
+motor2_Len = 7
+motor2_Ren = 8
+
+"""
+모터 드라이버에서 모터 구동부 전원은 따로 켜고 끌 수 있어요. Vcc는 모터 드라이버 전원이고 enable은 모터 구동부 전원입니다.
+목요일에 테스트했을 때는 Len과 Ren 신호를 둘 다 빵판의 5v에 연결해두고 썼는데, 이렇게 하면 모터 구동부 전원이 항상 켜져 있게 돼요.
+위의 코드처럼 Len과 Ren 신호를 빵판 말고 아두이노 핀에 연결해서, 쓸 때는 1로 안 쓸 때는 0으로 제어하면 배터리를 절약할 수 있어요.
+예시) 전진할 때는 Len은 1, Ren은 0으로 두기.
+"""
+
+motor1 = Motor(board, motor1_Lpwm, motor1_Rpwm, motor1_Len, motor1_Ren)
+motor2 = Motor(board, motor2_Lpwm, motor2_Rpwm, motor2_Len, motor2_Ren)
+
+# 위의 코드 이렇게 줄일 수 있어요.
+"""
 motor1 = Motor(board, 5, 6, 2, 4)
 motor2 = Motor(board, 9, 10, 7, 8)
+"""
 
 it.start()
 
